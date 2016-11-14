2f69c88db062498726f292bbf58faf46db68623f
Phillip_Chan <phillip.chan@liferay.COM>
2016-11-14 13:43:01 -0800

    Updated dependencies. Replaced gulp-ruby-sass with gulp-sass. Updated index.html to be truly boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,45 @@
 // Include gulp & gulp plugins
-var gulp = require('gulp');
 var concat = require('gulp-concat');
-var sass = require('gulp-ruby-sass');
+var gulp = require('gulp');
+var minify = require('gulp-minify');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
 
 // Compile SASS
 gulp.task('sass', function() {
-    return sass('src/scss/*.scss', { style: 'compressed' })
-        .pipe(concat('app.css'))
-        .pipe(gulp.dest('css'))
-});
+	return gulp.src('src/scss/**/*.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(concat('app.css'))
+	.pipe(gulp.dest('css'))
+	});
 
+// Concat JS Files
 gulp.task('scripts', function() {
-    return gulp.src([
-            'src/js/*'
-        ])
-        .pipe(concat('app.js'))
-        .pipe(gulp.dest('js'));
+	return gulp.src([
+		'src/js/**/*.js'
+		])
+	.pipe(concat('app.js'))
+	.pipe(minify(
+		{
+			ext:{
+				min:'.min.js'
+			},
+			ignoreFiles: ['.combo.js', '-min.js']
+		}
+	))
+	.pipe(gulp.dest('js'));
 });
 
 // Watch for changes
 gulp.task('watch', function() {
-    // Watch .js files
-    gulp.watch('src/js/*.js', ['scripts']);
-    // Watch .scss files
-    gulp.watch('src/scss/*.scss', ['sass']);
+	// Watch .js files
+	gulp.watch('src/js/*.js', ['scripts']);
+	// Watch .scss files
+	gulp.watch('src/scss/*.scss', ['sass']);
 });
 
+
 // Default Task
-gulp.task('default', ['scripts', 'sass', 'watch']);
+gulp.task('default', function() {
+	runSequence('scripts', 'sass', 'watch');
+});
